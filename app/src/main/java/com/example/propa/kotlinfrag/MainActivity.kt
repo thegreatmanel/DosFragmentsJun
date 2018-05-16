6f0b39a0f70398ebdf14efcec31ef1c99d62ce60
Manel <propaner14@gmail.com>
2018-05-16 11:10:59 +0200

    Traducción  implementada. Todo funcionando correctamente.

--- app/src/main/java/com/example/propa/kotlinfrag/MainActivity.kt
+++ app/src/main/java/com/example/propa/kotlinfrag/MainActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-//prueba commit
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
